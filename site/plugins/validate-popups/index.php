f09add7436774dc32a86a661af86a008d9188682
Pro-Use <robertprouse@gmail.com>
2021-10-12 10:34:34 +0100

    Add validate-popups plugin

---
+++ site/plugins/validate-popups/index.php
@@ -0,0 +1,24 @@
+<?php
+
+Kirby::plugin('robprouse/validate-popups', [ 
+    'hooks' => [
+        'page.update:after' => function ($newPage, $oldPage) {
+            $popups = $newPage->popups()->toStructure();
+            $errors = 'false';
+            $f_timestamp = $newPage->from()->toDate();
+            $t_timestamp = $newPage->to()->toDate();
+            foreach($popups as $popup) {
+                if ($popup->date() != ""){
+                    $p_timestamp = $popup->date()->toDate();
+                    if ( $p_timestamp < $f_timestamp ||
+                            $p_timestamp > $t_timestamp) {
+                        $errors = 'true';
+                        break;
+                    }
+                }
+            }
+            $newPage->dateError() = $errors;
+        }
+      ],
+]);
+
