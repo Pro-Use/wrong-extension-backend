0a82bbc63f9c974ff3e16107d2f4f7eec3661065
Pro-Use <robertprouse@gmail.com>
2021-10-12 22:53:41 +0100

    disable hook

--- site/plugins/validate-popups/index.php
+++ site/plugins/validate-popups/index.php
@@ -2,26 +2,25 @@
 
 Kirby::plugin('robprouse/validate-popups', [ 
     'hooks' => [
-        'page.update:after' => function ($newPage) {
-            $site = $this->site();
-            $page = $site->pages()->findById($newPage->id());
-            $popups = $page->popups()->toStructure();
-            $errors = 'false';
-            $f_timestamp = $page->from()->toDate();
-            $t_timestamp = $page->to()->toDate();
-            foreach($popups as $popup) {
-                if ($popup->date() != ""){
-                    $p_timestamp = $popup->date()->toDate();
-                    if ( $p_timestamp < $f_timestamp ||
-                            $p_timestamp > $t_timestamp) {
-                        $errors = 'true';
-                        break;
-                    }
-                }
-            }
-            $page->update(['dateError'=>$errors]);
-//            $page->update(['test'=>'testing']);
-        }
+//        'page.update:after' => function ($newPage) {
+//            $site = $this->site();
+//            $page = $site->pages()->findById($newPage->id());
+//            $popups = $page->popups()->toStructure();
+//            $errors = 'false';
+//            $f_timestamp = $page->from()->toDate();
+//            $t_timestamp = $page->to()->toDate();
+//            foreach($popups as $popup) {
+//                if ($popup->date() != ""){
+//                    $p_timestamp = $popup->date()->toDate();
+//                    if ( $p_timestamp < $f_timestamp ||
+//                            $p_timestamp > $t_timestamp) {
+//                        $errors = 'true';
+//                        break;
+//                    }
+//                }
+//            }
+//            $page->update(['dateError'=>$errors]);
+//        }
       ],
 ]);
 
