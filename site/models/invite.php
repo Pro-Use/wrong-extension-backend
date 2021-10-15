44e0e35900a26ac0bf1692c7e6faefb65febd427
Pro-Use <rob@prou.se>
2021-10-15 12:54:15 +0200

    date validation

--- site/models/invite.php
+++ site/models/invite.php
@@ -2,21 +2,13 @@
 
 class InvitePage extends Page
 {
-    public function invalidDate() {
-        $popups = $this->popups()->toStructure();
-        $errors = 'false';
-        $f_timestamp = $this->from()->toDate();
-        $t_timestamp = $this->to()->toDate();
-        foreach($popups as $popup) {
-            if ($popup->date() != ""){
-                $p_timestamp = $popup->date()->toDate();
-                if ( $p_timestamp < $f_timestamp ||
-                        $p_timestamp > $t_timestamp) {
-                    $errors = 'true';
-                    break;
-                }
-            }
+    public function returnDates() {
+        $all_dates = [];
+        $from = new DateTime($this->from()->toDate('Y-m-d'));
+        $to = new DateTime($this->to()->toDate('Y-m-d'));
+        for($i = $from; $i <= $to; $i->modify('+1 day')){
+           $all_dates[] = $i->format("Y-m-d");
         }
-        return $errors;
+        return $all_dates;
     }
 }
