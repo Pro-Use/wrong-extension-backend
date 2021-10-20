7a56347f0958e6292b40a4884a456d389b9b23ee
Pro-Use <rob@prou.se>
2021-10-20 12:24:21 +0200

    filter by date

--- site/templates/invite.php
+++ site/templates/invite.php
@@ -7,7 +7,22 @@
 <?php if ($kirby->user()) {
     snippet('popup_preview');  
 } else {
-    
+//    $urlhash = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_STRING);
+//    $url = base64_decode($urlhash);
+//    $popups = $page->popups()->toStructure();
+//    foreach($popups as $popup) {
+//        if ($popup->url() == $url){
+//            echo $popup->url();?>
+            <iframe src="//<?=$url?>"
+                frameborder="0" 
+                marginheight="0" 
+                marginwidth="0" 
+                width="100%" 
+                height="100%" 
+                scrolling="auto"></iframe >
+            //<?php break;
+//        }
+//    }
 }
 ?>
 </body>
--- site/templates/invites.json.php
+++ site/templates/invites.json.php
@@ -1,5 +1,5 @@
 <?php
-
+ header("Access-Control-Allow-Origin: *");
 
 $data = $page->children()
     ->filter(function ($child) {
@@ -7,6 +7,7 @@ $data = $page->children()
   });
 
 $now = new DateTime("now");
+$now_string = $result = $now->format('Y-m-d');
 $popups_json = [];
 
 foreach($data as $article) {
@@ -20,23 +21,27 @@ foreach($data as $article) {
     $popup_json = [];
 
     foreach($popups as $popup) {
-        $popup_json[] = [
-            'url' => $popup->url(),
-            'fullscreen' => (string)$popup->fullscreen(),
-            'width' => (string)$popup->width(),
-            'height' => (string)$popup->height(),
-            'position' => (string)$popup->position(),
-            'time' => (string)$popup->time(),
-            'date' => (string)$popup->date(),
-        ];
+        $dates = explode(",", $popup->date());
+        foreach($dates as $date){
+            $date = trim($date);
+            if ($date == $now_string || $date == ""){
+                $popup_json[] = [
+                'title' => (string)$article->title(), 
+                'id' => base64_encode($popup->url()),
+                'url' => (string)$popup->url(),
+                'fullscreen' => (string)$popup->fullscreen(),
+                'width' => (string)$popup->width(),
+                'height' => (string)$popup->height(),
+                'position' => (string)$popup->position(),
+                'time' => (string)$popup->time(),
+                'dates' => $dates
+                ];
+                break;
+            }
+        }
     }
 
-    $popups_json[] = [
-      'title' => (string)$article->title(), 
-      'from'=> (string)$article->from(),
-      'to' => (string)$article->to(),
-      'popups'   => $popup_json,
-    ];
+    $popups_json[] = $popup_json;
   }
 }
 
