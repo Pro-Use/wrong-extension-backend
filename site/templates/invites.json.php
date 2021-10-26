24ac89c0e31915b5ca2206fcb799f5db87dc8137
Pro-Use <apiareby@nl1-ss22.a2hosting.com>
2021-10-26 14:14:11 +0200

    add next popup

--- site/templates/invites.json.php
+++ site/templates/invites.json.php
@@ -6,14 +6,14 @@ $data = $page->children()
     return $child->status() != 'draft';
   });
 
-$now = new DateTime("now");
-$now_string = $result = $now->format('Y-m-d');
+$now = new DateTime("now", new DateTimeZone('Europe/London'));
+$now_string = $now->format('Y-m-d');
 $popups_json = [];
 $next_popups = [];
 foreach($data as $article) {
     
-  $from = new DateTime($article->from());
-  $to = new DateTime($article->to());
+  $from = new DateTime($article->from(), new DateTimeZone('Europe/London'));
+  $to = new DateTime($article->to(), new DateTimeZone('Europe/London'));
     
   if ($from<= $now && $now <= $to){
       
@@ -23,7 +23,7 @@ foreach($data as $article) {
     foreach($popups as $popup) {
         $dates = explode(", ", $popup->date());
         sort($dates);
-        foreach($dates as &$date){
+        foreach($dates as $date){
 //            $date = trim($date);
             if ($date == $now_string || $date == ""){
                 $id = base64_encode($popup->url());
@@ -45,16 +45,23 @@ foreach($data as $article) {
                 break;
             }
         }
+        $now = new DateTime($now_string, new DateTimeZone('Europe/London'));
         if ($to > $now) {
             foreach($dates as $date){
-                $popup_date = new DateTime($date);
+                if ($date == "") {
+                    $popup_date = clone $now;
+                    $popup_date->modify('+1 day');
+                } else {
+                    $popup_date = new DateTime($date, new DateTimeZone('Europe/London'));
+                }
                 if ($popup_date > $now) {
                     $next_popups[] = [
                     'time' => (string)$popup->time(),
-                    'date' => (string)$date,
-                    'popup'=> $popup
+                    'date' => $popup_date,
+                    'popup_set' => $article,
+                    'popup'=> $popup,
                     ];
-                break;
+                    break;
                 }
 
             }
@@ -73,27 +80,34 @@ usort($next_popups, function($a, $b) {
     }
     return $retval;
 });
-
-$next_popup = $next_popups[0];
-$next_popup_json = [
-                'curator' => (string)$next_popup['popup']->title(),
-                'title' => (string)$next_popup['popup']->popupSetTitle(),
-                'popup_title' => (string)$next_popup['popup']->popup_title(),
-                'popup_info' => (string)$next_popup['popup']->popup_text(), 
-                'id' => (string)$id,
-                'url' => (string)$next_popup['popup']->url(),
-                'info_url' => (string)$next_popup['popup']->url()."?id=".$id,
-                'fullscreen' => (string)$next_popup['popup']->fullscreen(),
-                'width' => (string)$next_popup['popup']->width(),
-                'height' => (string)$next_popup['popup']->height(),
-                'position' => (string)$next_popup['popup']->position(),
-                'time' => (string)$next_popup['popup']->time(),
-                'dates' => $next_popup['date']
-                ];
+if (count($next_popups) == 0) {
+    $next_popup_json = Null;
+} else {
+    $next_popup = $next_popups[0];
+    $next_popup_id = base64_encode($next_popup['popup']->url());
+    $next_popup_diff = (int)$now->diff($next_popup['date'])->format('%a');
+    $next_popup_json = [
+                    'curator' => (string)$next_popup['popup_set']->title(),
+                    'title' => (string)$next_popup['popup_set']->popupSetTitle(),
+                    'popup_title' => (string)$next_popup['popup']->popup_title(),
+                    'popup_info' => (string)$next_popup['popup']->popup_text(), 
+                    'id' => (string)$next_popup_id,
+                    'url' => (string)$next_popup['popup']->url(),
+                    'info_url' => (string)$next_popup['popup']->url()."?id=".$next_popup_id,
+                    'fullscreen' => (string)$next_popup['popup']->fullscreen(),
+                    'width' => (string)$next_popup['popup']->width(),
+                    'height' => (string)$next_popup['popup']->height(),
+                    'position' => (string)$next_popup['popup']->position(),
+                    'time' => (string)$next_popup['popup']->time(),
+                    'dates' => (string)$next_popup['date']->format('Y-m-d'),
+                    'diff' => $next_popup_diff
+                    ];
+}
 
 $json = ['lastUpdate' => (string)$page->lastUpdated(),
          'popups' => $popups_json,
-         'next_popup' => $next_popups
+         'next_popup' => $next_popup_json
+//        _json
         ];
 
 echo json_encode($json);
