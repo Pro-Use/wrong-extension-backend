95a1104669b48c77634caa668fb8383f56b81a5b
pro-use <robertprouse@gmail.com>
2023-05-31 17:10:38 +0100

    add dates and times to popups

--- site/config/config.php
+++ site/config/config.php
@@ -46,7 +46,58 @@ return [
         }
       }
   ],
-          
+  'routes' => function ($kirby) {
+      return [
+        [
+          'pattern' => 'archive.json',
+          'action'  => function () {
+            $page = $kirby()->page('invites');
+            $data = $page->children()
+              ->filter(function ($child) {
+              return $child->status() != 'draft';
+            });
+            $all_sets = [];
+            foreach($data as $set) {
+              $from = new DateTime($set->from(), new DateTimeZone('Europe/London'));
+              $to = new DateTime($set->to(), new DateTimeZone('Europe/London'));
+              $set_info = [
+                'from' => (string)$from,
+                'to' => (string)$to,
+                'curator' => (string)$set->title(),
+                'title' => (string)$set->popupSetTitle(),
+                'text' => (string)$set->popupSetText()
+              ];
+              $popups = $set->popups()->toStructure();
+              $popups_json = [];
+              foreach($popups as $popup) {
+                $dates = explode(", ", $popup->date());
+                sort($dates);
+                if($dates[0] == ''){
+                  $dates = [$from->format('Y-m-d')];
+                }
+                $id = base64_encode($popup->url());
+                $popups_json[] = [
+                  'popup_title' => (string)$popup->popup_title(),
+                  'popup_info' => (string)$popup->popup_text(), 
+                  'id' => (string)$id,
+                  'url' => (string)$popup->url(),
+                  'info_url' => (string)$set->url()."?id=".$id,
+                  'fullscreen' => (string)$popup->fullscreen(),
+                  'width' => (string)$popup->width(),
+                  'height' => (string)$popup->height(),
+                  'position' => (string)$popup->position(),
+                  'popup_date' => $dates[0],
+                  'popup_time' => (string)$popup->time(),
+                ];
+              }
+              $set_info['popups'] = $popups_json;
+              $all_sets[] = $set_info;
+            }
+          return $all_sets;
+          }
+        ]
+      ];
+  },          
 //  Restrict Access
   'sylvainjule.bouncer.list' => [
      'curator' => [
