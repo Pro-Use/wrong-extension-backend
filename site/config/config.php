b94d5734d7477193b66bb08a8fab4f75b1502c02
Pro-Use <robertprouse@gmail.com>
2023-08-03 11:56:52 +0100

    Check days and day in popup sets and make sure popup is archivable

--- site/config/config.php
+++ site/config/config.php
@@ -56,42 +56,45 @@ return [
             $data = kirby()->site()->archive()->toPages();
             $all_sets = [];
             foreach($data as $set) {
-              $from = new DateTime($set->from(), new DateTimeZone('Europe/London'));
-              if ($set->archiveImage()->isNotEmpty()){
-                $archiveImage = $set->archiveImage()->toFile()->srcset([350, 700]);
-              } else {
-                $archiveImage = false;
-              }
-              $set_info = [
-                'from' => (string)$from->format('d-m-Y'),
-                'curator' => (string)$set->title(),
-                'title' => (string)$set->popupSetTitle(),
-                'text' => (string)$set->popupSetText(),
-                'project' => (string)$set->slug(),
-                'img' => $archiveImage,
-              ];
-              $popups = $set->popups()->toStructure();
-              $popups_json = [];
-              foreach($popups as $popup) {
-                $dates = explode(", ", $popup->date());
-                $id = base64_encode($popup->url());
-                $popups_json[] = [
-                  'popup_title' => (string)$popup->popup_title(),
-                  'popup_info' => (string)$popup->popup_text(), 
-                  'id' => (string)$id,
-                  'url' => (string)$popup->url(),
-                  'info_url' => (string)$set->url()."?id=".$id,
-                  'fullscreen' => (string)$popup->fullscreen(),
-                  'width' => (string)$popup->width(),
-                  'height' => (string)$popup->height(),
-                  'position' => (string)$popup->position(),
-                  'popup_day' => (string)$popup->day(),
-                  'popup_time' => (string)$popup->time(),
+              if ($set->days()->isNotEmpty()){
+                $from = new DateTime($set->from(), new DateTimeZone('Europe/London'));
+                if ($set->archiveImage()->isNotEmpty()){
+                  $archiveImage = $set->archiveImage()->toFile()->srcset([350, 700]);
+                } else {
+                  $archiveImage = false;
+                }
+                $set_info = [
+                  'from' => (string)$from->format('d-m-Y'),
+                  'days' => $set->days()->toInt(),
+                  'curator' => (string)$set->title(),
+                  'title' => (string)$set->popupSetTitle(),
+                  'text' => (string)$set->popupSetText(),
+                  'project' => (string)$set->slug(),
+                  'img' => $archiveImage,
                 ];
+                $popups = $set->popups()->toStructure();
+                $popups_json = [];
+                foreach($popups as $popup) {
+                  $id = base64_encode($popup->url());
+                  if ($popup->day()->isNotEmpty() && $popup->archivable()->toBool()){
+                    $popups_json[] = [
+                      'popup_title' => (string)$popup->popup_title(),
+                      'popup_info' => (string)$popup->popup_text(), 
+                      'id' => (string)$id,
+                      'url' => (string)$popup->url(),
+                      'info_url' => (string)$set->url()."?id=".$id,
+                      'fullscreen' => (string)$popup->fullscreen(),
+                      'width' => (string)$popup->width(),
+                      'height' => (string)$popup->height(),
+                      'position' => (string)$popup->position(),
+                      'popup_day' => (string)$popup->day(),
+                      'popup_time' => (string)$popup->time(),
+                    ];
+                  }
+                }
+                $set_info['popups'] = $popups_json;
+                $all_sets[] = $set_info;
               }
-              $set_info['popups'] = $popups_json;
-              $all_sets[] = $set_info;
-              
             }
           echo json_encode($all_sets);
           }
